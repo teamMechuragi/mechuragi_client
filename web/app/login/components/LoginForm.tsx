9f4876c8596911fe2272a484deea7728f20331c5
catapillar0505 <62907792+catapillar0505@users.noreply.github.com>
2026-02-25 17:06:03 +0900

    fix: useeffect 임포트

--- web/app/login/components/LoginForm.tsx
+++ web/app/login/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import { useRouter } from "next/navigation";
 import SocialLogin from "./SocialLogin";
